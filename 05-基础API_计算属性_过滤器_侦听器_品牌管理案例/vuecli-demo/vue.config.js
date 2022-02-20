f4690cea2b27845c1d5d5d8c050add573c62e68e
zuoliang <193957056@qq.com>
2022-02-21 00:51:46 +0800

    1.21.1

---
+++ 05-基础API_计算属性_过滤器_侦听器_品牌管理案例/vuecli-demo/vue.config.js
@@ -0,0 +1,21 @@
+// vue脚手架项目-默认的配置文件名
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//         transpileDependencies: true
+//     }
+//     // ,
+//     //  {
+//     //     devServer: {
+//     //         port: 3000,
+//     //         open: true //浏览器自动打开
+//     //     }
+//     // }
+// )
+
+module.exports = {
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    lintOnSave: false
+}
