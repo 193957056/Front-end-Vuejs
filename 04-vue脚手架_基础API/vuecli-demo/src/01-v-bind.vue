f0ae48b328e0e4ee2d85e2df7e31a2f8864b03b2
zuoliang <193957056@qq.com>
2022-02-20 19:16:57 +0800

    1.20

---
+++ 04-vue脚手架_基础API/vuecli-demo/src/01-v-bind.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+<!-- v-bind -->
+<a v-bind:href="url">我是a标签</a>
+<img :src="imgUrl">
+  </div>
+</template>
+
+<script>
+export default {
+  // 1.变量在data函数return的对象上
+data(){
+  return{
+    url:"www.baidu.com",
+    imgUrl:"https://img95.699pic.com/photo/50136/1351.jpg_wh300.jpg"
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ 04-vue脚手架_基础API/vuecli-demo/src/02-v-on.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <!-- 1.绑定事件 -->
+  <p>你要购买商品的数量:{{count}}</p>
+  <button v-on:click="count = count +1">+1</button><br>
+  <button v-on:click="addFn">+1</button><br>
+  <button v-on:click="addCountFn(5)">+5</button><br>
+  <button @click="subFn">-1</button>
+
+</div>
+  
+</template>
+
+<script>
+export default {
+  // 1.变量在data函数return的对象上
+data(){
+  return{
+    count:1
+  }
+},
+// 2.定义函数
+methods:{
+  addFn(){
+    this.count ++;
+  },
+  addCountFn(num){
+   this.count = this.count + num;
+  },
+  subFn(){
+  this.count--
+}
+},
+
+}
+</script>
+
+<style>
+
+</style>
