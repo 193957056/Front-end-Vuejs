ba199f56a3d6455461781bf10eaf121b34ae49ac
zuoliang <193957056@qq.com>
2022-03-02 23:18:15 +0800

    3.3.1

---
+++ 07_生命周期_axios_购物车案例/shopcar/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <MyHeader background="red" title="购物车案例"></MyHeader>
+    <MyGoods></MyGoods>
+    <MyFooter></MyFooter>
+  </div>
+</template>
+<script>
+
+import MyHeader from './components/MyHeader.vue'
+import MyFooter from './components/MyFooter.vue'
+import MyGoods from './components/MyGoods.vue'
+export default {
+components:{
+  MyHeader,
+  MyFooter,
+  MyGoods
+}
+  
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ 07_生命周期_axios_购物车案例/vuecli-demo/README.md
@@ -0,0 +1,32 @@
+# vuecli-demo
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+### 安装moment
+```
+yarn add moment
+```
+### 安装bootstrap
+```
+yarn add bootstrap
+```
