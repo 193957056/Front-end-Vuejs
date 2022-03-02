5d95751ed897ef2ccf9d65cf0ec9c9119437c7df
zuoliang <193957056@qq.com>
2022-03-03 00:25:27 +0800

    3.3.3

--- 07_生命周期_axios_购物车案例/shopcar/src/components/MyGoods.vue
+++ 07_生命周期_axios_购物车案例/shopcar/src/components/MyGoods.vue
@@ -2,19 +2,28 @@
   <div class="my-goods-item">
     <div class="left">
       <div class="custom-control custom-checkbox">
-        <input type="checkbox" class="custom-control-input" id="input"
+        <!-- *重要:
+          每个对象和组件都是独立的
+          对象里的goods_state关联自己对应商品的复选框
+         -->
+         <!-- bug:
+          循环的所有label的for都是input, id也都是input - 默认只有第一个生效
+          解决: 每次对象里的id值(1, 2), 分别给id和for使用即可区分
+          -->
+        <input type="checkbox" class="custom-control-input" :id="gObj.id"
+        v-model="gObj.goods_state"
         >
-        <label class="custom-control-label" for="input">
-          <img src="http://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" alt="">
+        <label class="custom-control-label" :for="gObj.id">
+          <img :src="gObj.goods_img" alt="">
         </label>
       </div>
     </div>
     <div class="right">
-      <div class="top">商品名字</div>
+      <div class="top">{{ gObj.goods_name }}</div>
       <div class="bottom">
-        <span class="price">¥ 100</span>
+        <span class="price">¥ {{ gObj.goods_price }}</span>
         <span>
-            <MyCount></MyCount>
+            <MyCount :obj="gObj"></MyCount>
         </span>
       </div>
     </div>
@@ -22,11 +31,14 @@
 </template>
 
 <script>
-import MyCount from './MyCount.vue'
+import MyCount from './MyCount'
 export default {
-components:{
-    MyCount
-}
+    props: {
+      gObj: Object
+    },
+    components: {
+        MyCount
+    }
 }
 </script>
 
