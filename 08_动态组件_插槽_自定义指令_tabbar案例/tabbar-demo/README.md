364af3f518f1b0916d932d01b4e3cb9cdebbc247
zuoliang <193957056@qq.com>
2022-03-08 01:20:12 +0800

    3.7

--- 08_动态组件_插槽_自定义指令_tabbar案例/tabbar-demo/README.md
+++ 08_动态组件_插槽_自定义指令_tabbar案例/tabbar-demo/README.md
@@ -22,3 +22,13 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 下载包
+```
+yarn add less less-loader@5.0.0 -D
+yarn add bootstrap axios
+```
+### 启动项目
+```
+yarn serve
+```
--- 08_动态组件_插槽_自定义指令_tabbar案例/tabbar-demo/src/main.js
+++ 08_动态组件_插槽_自定义指令_tabbar案例/tabbar-demo/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css"
+import "./assets/fonts/iconfont.css" //引入字体图标
 
 Vue.config.productionTip = false
 
+Vue.directive('focus', {
+    inserted(el) {
+        el.focus()
+    }
+})
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
